4be9dcf07199822334284bd4aad70015eadaaea7
giver-kdk <giverbro13@gmail.com>
2023-10-25 21:13:08 +0545

    ims app startup setup

---
+++ ims/src/App.js
@@ -0,0 +1,15 @@
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+		Sidebar Component Here
+		{/* <Routes> */}
+			{/* <Route path="/" element={<Dashboard/>}/> */}
+			{/* <Route path="/products" element={<Products/>}/> */}
+		{/* </Routes> */}
+    </div>
+  );
+}
+
+export default App;
