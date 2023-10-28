602e745bc65471b98caa9d77a84405e36bff9001
Nirajan1-droid <nirajanbhattarai12345@gmail.com>
2023-10-28 09:07:20 +0545

    Giver Branch

--- ims/src/components/common/info_card/index.jsx
+++ ims/src/components/common/info_card/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import "./style.scss"
 
 export default function InfoCard(props) {
-    let InfoElem = props.info.map((data, index) =>{
+    let InfoElem = props.info.map((data, index, array) =>{
         return (
             <div className="infocard__data">
                 <div className="infocard__data--info">
 					<h2>{data.value}</h2>
 					<h3>{data.attribute}</h3>
+                    {(index !=1 && index != array.length - 1) && <div className='v__line'></div>}
 				</div>
             </div>
         );
