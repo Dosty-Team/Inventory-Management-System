35ed3337c6877f64bd906574c912f0ca226ed071
Nirajan1-droid <nirajanbhattarai12345@gmail.com>
2023-10-31 23:08:26 +0545

    a

--- src/store/index.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import pageSlice from "./pageSlice";
-
-const store = configureStore({
-	reducer:{
-		page: pageSlice.reducer
-	}
-});
-export default store;
