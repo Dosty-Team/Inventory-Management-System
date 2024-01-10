0ef2b0de896c1ccb514ea50c1c4ab5d537cac099
giver-kdk <giverbro13@gmail.com>
2024-01-10 23:09:57 +0545

    no change

--- ims/src/components/products/index.jsx
+++ ims/src/components/products/index.jsx
@@ -1,86 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { pageActions } from "../../store/pageSlice";
-import { productCol} from './data';https://github.com/Dosty-Team/Inventory-Management-System.git      console.error('Error fetching data from server:', error.message);
-    }
-    
-  }, [shouldProductRender]);
-  dispatch(pageActions.setProducts());
-  
-  console.log('productpage overview', productOverviewd);
+import { productCol,productOverview} from './data';
+import { Table, Space } from 'antd';
+import InfoCard from "../common/info_card";
+import Pop from './pop';
+import "./style.scss";
+import axios from 'axios';
+
+export default function Product() {
+	let dispatch = useDispatch();
+	const [productData, setProductData] = useState(null);
+	const [productOverviewd, setProductOverviewd] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/v1/productin');
+        setProductData(response.data.products);
+		setProductOverviewd(response.data.productOverview);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data from server:', error.message);
+      }
+    };
+
+    fetchData();
+    dispatch(pageActions.setProducts());
+  }, [dispatch]);
+
+  console.log('productpage productOverviewdproductOverviewdproductOverviewdproductOverviewdproductOverviewdproductOverviewdproductOverviewd', productOverviewd);
 	
-// for EditProduct
-// const handleEditSuccess = (newName) => {
-//   setCategoryName(newName);
-// };
-const handleEditProduct = (newName, productKey) => {
-  // Update the product name in the productData state
-  const updatedProductData = productData.map((product) => {
-    if (product.key === productKey) {
-      return { ...product, productName: newName };
-    }
-    return product;
-  });
-
-  // Set the updated productData state
-  setProductData(updatedProductData);
-};
-
- const productColumn = [
-  {
-      title: 'SN',
-      dataIndex: 'key',
-      key: 'sn',
-  },
-  {
-      title: 'Product Name',
-      dataIndex: 'productName',
-      key: 'productName',
-  },
-  {
-      title: 'Quantity',
-      dataIndex: 'qty',
-      key: 'qty',
-  },
-  {
-      title: 'Category',
-      dataIndex: 'category',
-      key: 'category',
-  },
-  {
-      title: 'In-Stock',
-      dataIndex: 'inStock',
-      key: 'inStock',
-      
-  },
-  {
-      title: 'Cost Price',
-      dataIndex: 'costPrice',
-      key: 'costPrice',
-  },
-  {
-      title: 'Selling Price',
-      dataIndex: 'sellingPrice',
-      key: 'sellingPrice',
-  },
-  {
-      title: 'Product Added',
-      dataIndex: 'addedDate',
-      key: 'addedDate',
-  },
-  {
-      title: 'Action',
-      key: 'action',
-      render: (_, record) => (
-        <Space size="middle">
-          
-          <EditProduct product={record} updateProductName={handleEditProduct} />
-
-          <a>Delete</a>
-        </Space>
-      ),
-    }
-];
   return (
 	<div className='product'>
 		<div className="product__inventory flex__row hard__shadow">
@@ -89,7 +38,7 @@ const handleEditProduct = (newName, productKey) => {
 			</div>
 		<div className="product__list hard__shadow">
 			<h2>Products</h2>
-			<Table columns={productColumn} dataSource={productData} pagination={{onChange(current){setPage(current);}}}/>
+			<Table columns={productCol} dataSource={productData}/>
 		</div>
 	</div>
   )
