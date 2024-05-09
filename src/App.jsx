70366f85eceb1735b761620472b0fa47a540cb6e
Nirajan <65585311+Nirajan1-droid@users.noreply.github.com>
2024-05-10 01:20:54 +0545

    ch
    ch

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App flex__row">
 		{/* i am from the maain branch testing hte merge thing. */}
-		{/* iwant to apply the frontend changes to the frontend-1  branch */}
+		{/* iwant to apply the frontend changes to the frontend-1  branchsss */}
 		<Sidebar/>
 		<div className="page__wrapper flex__col">
 			<Topbar/>
